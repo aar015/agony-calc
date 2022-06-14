abf08b08f48172135424675a17849385d2f5c15e
Avi Revah <avirlincoln@gmail.com>
2022-06-13 21:10:05 -0500

    Add immovable object option

--- scripts.js
+++ scripts.js
@@ -1,24 +1,40 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("calculate").addEventListener("click", () => {
-        const life = 1. * document.getElementById("life").value;
-        const n = Math.ceil((Math.sqrt(1. + 8. * life) - 1.) / 2.);
-        const card = 1. * document.getElementById("card").value;
-        const agony = 1. * document.getElementById("agony").value;
-        let result = 10000.;
-        if (agony < n) {
-            result = 0;
-        } else {
-            for(let i=1.; i <= card; i++) {
-                result *= (agony - n + i) / (agony + i);
+function calculate() {
+    const life = 1. * document.getElementById("life").value;
+    let n;
+    if (document.getElementById("weapon").checked) {
+        n = 0;
+        let damage = 0;
+        while (true) {
+            n++;
+            damage += Math.ceil(n / 2.);
+            if (damage >= life) {
+                break;
             }
         }
-        result = Math.round(result) / 100.;
-        document.getElementById("output").innerText = result + "%  chance of lethal";
-        if (result < 50) {
-            document.getElementById("output").className = "bad";
+    } else {
+        n = Math.ceil((Math.sqrt(1. + 8. * life) - 1.) / 2.)
+    }
+    const card = 1. * document.getElementById("card").value;
+    const agony = 1. * document.getElementById("agony").value;
+    let result = 10000.;
+    if (agony < n) {
+        result = 0;
+    } else {
+        for(let i=1.; i <= card; i++) {
+            result *= (agony - n + i) / (agony + i);
         }
-        else {
-            document.getElementById("output").className = "good";
-        }
-    });
+    }
+    result = Math.round(result) / 100.;
+    document.getElementById("output").innerText = result + "%  chance of lethal";
+    if (result < 50) {
+        document.getElementById("output").className = "bad";
+    }
+    else {
+        document.getElementById("output").className = "good";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    calculate();
+    document.getElementById("calculate").addEventListener("click", calculate);
 });
